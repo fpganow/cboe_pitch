618b6669c5570e2405436d5f7c7957eaf5247b16
John Stratoudakis <johnstratoudakis@gmail.com>
2025-02-08 12:52:02 -0500

    Parser now supports pcap file and shows parsed raw packets and BATS messages, nicely formatted

--- cboe_pitch/file_parser.py
+++ cboe_pitch/file_parser.py
@@ -1,28 +1,42 @@
+import logging
 from pathlib import Path
 from typing import List
 
+from scapy.all import rdpcap, IP, UDP
 from .seq_unit_header import SequencedUnitHeader
+from .util import get_line, get_form
+
+
+logger = logging.getLogger(__name__)
 
 
 class FileParser:
     @staticmethod
-    def parse_file(file_path: str) -> List[SequencedUnitHeader]:
-        f_in = Path(file_path)
-        if f_in.exists() is False:
+    def parse_pcap(file_path: str,
+                   dst_ip: str,
+                   dport: int) -> List[SequencedUnitHeader]:
+
+        if Path(file_path).exists() is False:
             raise Exception(f"File {file_path} does not exist")
 
-        rem_bytes = Path(file_path).read_bytes()
-
+        # Use scapy to parse pcap file
+        logger.warn(get_form("Parsed packets:"))
+        logger.warn(get_line("-", "+"))
         out_arr = []
+        packets = rdpcap(file_path)
+        for packet in packets:
+            if IP in packet and packet[IP].dst == dst_ip and packet[UDP].dport == dport:
+                logger.warn(get_form(packet.summary()))
+                rem_bytes = packet[UDP].payload.load
 
-        more_seq = True
-        while more_seq:
-            [seq_unit_hdr, rem_bytes] = SequencedUnitHeader.from_bytestream(
-                msg_bytes=rem_bytes
-            )
-            out_arr.append(seq_unit_hdr)
+                more_seq = True
+                while more_seq:
+                    [seq_unit_hdr, rem_bytes] = SequencedUnitHeader.from_bytestream(
+                        msg_bytes=rem_bytes
+                    )
+                    out_arr.append(seq_unit_hdr)
 
-            if rem_bytes is None:
-                break
+                    if rem_bytes is None:
+                        break
 
         return out_arr
